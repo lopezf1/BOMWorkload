9a20c2996a557298568815ccf0db760f3cfec150
Fernando Lopez <lopezf1@sbcglobal.net>
2015-09-03 20:52:25 -0700

    Reduced code to just work cycle-time.

--- bom_cycletime.R
+++ bom_cycletime.R
@@ -1,32 +1,47 @@
+# Just focus on work cycle-time.  Assume all inbox cycle-time
+# handled wihtin one day.
+
 setwd("~/RProgramming/BOMWorkload")
 
 library(dplyr)
 library(tidyr)
-library(xlsx)
-library(ggplot2)
 
-# Make sure file is saved a comma separate value file.
-df <- read.csv("E-CASCADE - Incoming Volumes with Revenue and Resubmit Reasons.csv",
-               header=TRUE, sep=",")
-employee <- read.xlsx("employee_region.xlsx", 1, header=TRUE)
-headcount <- read.xlsx("production_hc.xlsx", 1, header=TRUE)
-headCountReg <- filter(headcount, !Region=="Total") #Huh?????
-headCountTotal <- filter(headcount, Region=="Total") #Huh????
+# Read in employee file for regional analysis.
 
-# Get ride of unnecessary variables and rename columns.
-dfReduced <- select(df, c(2, 13, 16,17,19,25,27,44,46))
-names(dfReduced) <- c("ID", "Date", "Resubmit", "Audit", "Lead",
-                      "Account", "TOV", "BOM", "Indicator")
+employee <- read.csv("employee_region.csv",
+                     header=TRUE, sep=",") %>%
+    separate(col="Name", into=c("BOM", "first"), sep=",") %>%
+    select(-first)
 
-dfReduced <- dfReduced %>%
-    merge(employee, by.x="BOM", by.y="Name") %>%
-    separate(col="BOM", into=c("BOM", "first1"), sep=",") %>%
-    separate(col="Lead", into=c("Lead", "first2"), sep=",") %>%
-    select(-c(first1, first2))
+# Read in Work Cycle-time file, select specific columns and rename.
 
-dfReduced$newDate <- dfReduced$Date %>%
-    strptime(format=("%m/%d/%Y %H:%M")) %>%
-    format("%y-%m") %>%
-    as.factor()
+dfWork <- read.csv("E-CASCADE - Work Cycle Time.csv",
+               header=TRUE, sep=",") %>%
+    select(c(7, 4, 13, 19, 22, 5))
+names(dfWork) <- c("ID", "Date", "Status", "Lead",
+                   "BOM", "Interval")
 
-rm(df)
+# Simplify BOM names and combine employee and cycle-time files.
+
+dfWork <- dfWork %>%
+separate(col="BOM", into=c("BOM", "first1"), sep=",") %>%
+separate(col="Lead", into=c("Lead", "first2"), sep=",") %>%
+select(-c(first1, first2)) %>%
+merge(employee, by.x="BOM", by.y="BOM")  
+
+# Fix up the dates.
+
+dfWork$newDate <- dfWork$Date %>%
+strptime(format=("%m/%d/%Y %H:%M")) %>%
+format("%y-%m") %>%
+as.factor()
+
+# Now create a table of values.
+
+# Create plot for workcycle-time in July.
+box <- dfWork %>% 
+    filter(newDate=="15-07" & Region=="Named") %>% 
+    as.data.frame()
+
+boxplot(Interval ~ Region, data=box)
+str(box)
